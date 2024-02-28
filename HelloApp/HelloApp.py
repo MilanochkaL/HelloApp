4d1340fb6ad8fa80746e4a8c3163c87cb1d166a2
Milana <Milka.lokteva@yandex.ru>
2024-02-28 11:33:49 +0300

    adding an algorithm to compare two dates

--- HelloApp/HelloApp.py
+++ HelloApp/HelloApp.py
@@ -4,3 +4,16 @@ s = "/\\"*30
 print(s)
 print("New project")
 print(s)
+while True:
+    try:
+        import datetime
+
+        date1 = datetime.datetime.strptime(input("введите 1 дату образца dd.mm.yyyy: "), "%d.%m.%Y")
+        date2 = datetime.datetime.strptime(input("введите 2 дату Образца dd.mm.yyyy:"), "%d.%m.%Y")
+        if (date1 < date2):
+            print("yes")
+        else:
+            print("no")
+        break
+    except ValueError:
+        print("некорректный ввод")
